f99b4ebb84b0f8e23ae0afae8d40f2732d3fc99b
Ian Holmes <github@yam.biowiki.org>
2016-05-24 16:35:15 -0700

    Viterbi prototypes

--- src/viterbi.h
+++ src/viterbi.h
@@ -47,12 +47,39 @@ struct MutatorScores {
 
 class MutatorMatrix {
 private:
+  size_t maxDupLen;
   vguard<LogProb> cell;
+
+  static inline size_t nCells (size_t seqLen) {
+    return (maxDupLen + 2) * (seqLen + 1);
+  };
+
+  inline size_t sCellIndex (Pos pos) const {
+    return (maxDupLen + 2) * pos;
+  };
+  inline size_t dCellIndex (Pos pos) const {
+    return (maxDupLen + 2) * pos + 1;
+  };
+  inline size_t fCellIndex (Pos pos, Pos idx) const {
+    return (maxDupLen + 2) * pos + 1 + idx;
+  };
+
 public:
   const Machine& machine;
-  const MutatorScores& scores;
+  const InputModel& inputModel;
+  const MutatorParams& mutatorParams;
   const TokSeq& seq;
-  MutatorMatrix (const MutatorScores& scores, const TokSeq& seq);
+  MachineScores machineScores;
+  MutatorScores mutatorScores;
+
+  MutatorMatrix (const Machine& machine, const InputModel& inputModel, const MutatorParams& mutatorParams, const TokSeq& seq);
+
+  inline LogProb& sCell (Pos pos) { return cell[sCellIndex(pos)]; }
+  inline LogProb& dCell (Pos pos) { return cell[dCellIndex(pos)]; }
+  inline LogProb& fCell (Pos pos, Pos idx) { return cell[fCellIndex(pos,idx)]; }
+  inline const LogProb sCell const (Pos pos) { return cell[sCellIndex(pos)]; }
+  inline const LogProb dCell const (Pos pos) { return cell[dCellIndex(pos)]; }
+  inline const LogProb fCell const (Pos pos, Pos idx) { return cell[fCellIndex(pos,idx)]; }
 };
 
 struct ViterbiMatrix {
