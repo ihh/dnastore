18cc457046b73f73c30d54c7a2040f2536d51e24
Ian Holmes <ihh@berkeley.edu>
2016-05-26 00:25:57 -0700

    bigrat

--- doc/mixradar.pl
+++ doc/mixradar.pl
@@ -1,15 +1,19 @@
 #!/usr/bin/perl -w
 
+use Math::BigRat;
+
 die "Usage: $0 <string> <eofprob> radix1 radix2 radix3..." unless @ARGV > 2;
-my ($text, $peof, @radix) = @ARGV;
-$peof = eval($peof);
+my ($text, $peofStr, @radix) = @ARGV;
+my $peof = Math::BigRat->new ($peofStr);
+my $pbit = (1 - $peof) / 2;
+#warn "pbit=$pbit peof=$peof";
 
 my %weight = ('0' => (1-$peof)/2,
 	      '1' => (1-$peof)/2,
 	      'x' => $peof);
 
 my ($pmin, $pmax) = getRange ($text, %weight);
-# warn "pmin=$pmin pmax=$pmax";
+#warn "pmin=$pmin pmax=$pmax";
 my @out = encode ($pmin, $pmax, @radix);
 print "@out\n";
 
@@ -24,7 +28,7 @@ sub getRange {
 	$pmin{$c} = $p;
 	$p += $weight{$c} / $norm;
 	$pmax{$c} = $p;
-#	warn "pmin{$c}=$pmin{$c} pmax{$c}=$pmax{$c}";
+#	warn "weight{c}=$weight{$c} pmin{$c}=$pmin{$c} pmax{$c}=$pmax{$c}";
     }
     my ($pmin, $pmax) = (0, 1);
     for my $c (split(//,$seq)) {
@@ -44,6 +48,7 @@ sub encode {
     my @out;
     for my $radix (@radix) {
 	my @q = map ($qmin + $_*($qmax-$qmin)/$radix, 0..$radix);
+#	warn "q=(@q) (pmin,pmax)=($pmin,$pmax) p=$p";
 	my ($digit) = grep ($q[$_] <= $p && $q[$_+1] > $p, 0..$#q);
 	push @out, $digit."_".$radix;
 	($qmin, $qmax) = @q[$digit,$digit+1];
