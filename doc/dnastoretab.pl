32882f8dd49f97c558683c6348cd3bebb93d71c2
Ian Holmes <ihh@berkeley.edu>
2016-05-28 18:17:20 -0700

    ./dnastoretab.

--- doc/dnastoretab.pl
+++ doc/dnastoretab.pl
@@ -6,21 +6,31 @@ use List::Util qw(sum);
 
 my $dnastore = "../bin/dnastore";
 
-my @params = ({ l=>2, c=>1 },
+my @params = ({ l=>2, c=>0, repeats=>1 },
+	      { l=>2, c=>0 },
+	      { l=>2, c=>1 },
 	      { l=>4, c=>2 },
 	      { l=>6, c=>4 },
 	      { l=>8, c=>4 },
 	      { l=>10, c=>4 },
 	      { l=>12, c=>4 });
 
+my %ignore = map (($_=>1), qw(note repeats));
+
 for my $param (@params) {
     my $l = $param->{l};
     my $c = $param->{c};
+    my $repeats = $param->{repeats} ? 'yes' : 'no';
+    my $reparg = $param->{repeats} ? '-t 0' : '';
+    my $note = $param->{note} || "";
     
     my $fh = File::Temp->new();
     my $filename = $fh->filename;
 
-    my @line = `$dnastore -l $l -c $c -S $filename -R --print-controls`;
+    my $args = join(" ",map("-$_ $param->{$_}",grep (!$ignore{$_}, keys %$param)));
+    my $command = "$dnastore --save-machine $filename --rate --print-controls $reparg $args";
+    warn $command, "\n";
+    my @line = `$command`;
 
     my @controls;
     if ($line[0] =~ /Control words: (.*)/) {
@@ -53,9 +63,5 @@ for my $param (@params) {
 	}
     }
 
-    print "\\\\hline\n";
-    print join (" & ", $l, $c, $nStates, $nTrans, sprintf("%.4g",$rate), $controls[0]), " \\\\\n";
-    for my $c (@controls[1..$#controls]) {
-	print join (" & ", "", "", "", "", "", $c), " \\\\\n";
-    }
+    print join (" & ", $l, $c, $repeats, $nStates, $nTrans, sprintf("%.4g",$rate), join (", ", map ("{\\tt $_}", @controls)), $note), " \\\\\n";
 }
