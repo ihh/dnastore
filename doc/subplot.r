b308554339df7248d60a81e14f3585073e33c015
Ian Holmes <ihh@berkeley.edu>
2016-06-02 16:27:14 -0700

    dupplot

--- doc/subplot.r
+++ doc/subplot.r
@@ -6,7 +6,7 @@ df.l4$group <- 1;
 df.l4.mix2$group <- 2;
 df.l4.ham$group <- 3;
 df <- rbind(df.l4,df.l4.mix2,df.l4.ham);
-df$Code=factor(df$group,labels=c("DNASTORE(4)","MIXRADAR(2) + DNASTORE(4)","HAMMING(7,4) + DNASTORE(4)"))
-subplot <- ggplot(df,aes(x=SubProb,y=MeanEditsPerBit,color=Code)) + geom_errorbar(aes(ymin=MeanEditsPerBit-2*StDevEditsPerBit,ymax=MeanEditsPerBit+2*StDevEditsPerBit)) + geom_line(aes(linetype=Code)) + geom_point(aes(shape=Code)) + ylab("Edits per bit") + xlab("Substitution probability per base") + ggtitle("Effect of substitutions on decoding accuracy") + theme(legend.position=c(.2,.9));
+df$Code=factor(df$group,labels=c("DNASTORE(4)","MIXRADAR(2) + DNASTORE(4)","HAMMING(7,4) + DNASTORE(4)"));
+subplot <- ggplot(df,aes(x=SubProb,y=MeanEditsPerBit,color=Code)) + geom_errorbar(aes(ymin=MeanEditsPerBit-2*StDevEditsPerBit,ymax=MeanEditsPerBit+2*StDevEditsPerBit)) + geom_point(aes(shape=Code),size=2) + ylab("Edits per bit") + xlab("Substitution probability per base") + ggtitle("Effect on decoding accuracy of point substitutions") + theme(legend.position=c(.2,.9)) + stat_smooth(method = "loess", formula = y ~ x, size = 1);
 ggsave("subplot.ps",plot=subplot);
 
