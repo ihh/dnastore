f6278fb60d80126eda135cb1ca4d67fb32f1b41d
Ian Holmes <ihh@berkeley.edu>
2016-06-03 10:26:47 -0700

    smoother fits

--- doc/dupplot.r
+++ doc/dupplot.r
@@ -6,6 +6,6 @@ df.l8$group <- 2;
 df <- rbind(df.l4,df.l8);
 df$Context=factor(df$group,labels=c("2 bases","4 bases"));
 g <- guide_legend(title="Error model context");
-dupplot <- ggplot(df,aes(x=DupProb,y=MeanEditsPerBit,color=Context)) + geom_errorbar(aes(ymin=MeanEditsPerBit-2*StDevEditsPerBit,ymax=MeanEditsPerBit+2*StDevEditsPerBit)) + geom_point(aes(shape=Context),size=2) + ylab("Edits per bit") + xlab("Duplication probability per base") + ggtitle("Effect on decoding accuracy of short (<5-base) duplications") + theme(legend.position=c(.2,.9)) + stat_smooth(method = "loess", formula = y ~ x, size = 1) + guides(colour=g,shape=g);
+dupplot <- ggplot(df,aes(x=DupProb,y=MeanEditsPerBit,color=Context)) + geom_errorbar(aes(ymin=MeanEditsPerBit-2*StDevEditsPerBit,ymax=MeanEditsPerBit+2*StDevEditsPerBit)) + geom_point(aes(shape=Context),size=2) + ylab("Edits per bit") + xlab("Duplication probability per base") + ggtitle("Effect on decoding accuracy of short (<5-base) duplications") + theme(legend.position=c(.2,.9)) + stat_smooth(method = "loess", formula = y ~ log(x), size = 1) + guides(colour=g,shape=g);
 ggsave("dupplot.ps",plot=dupplot);
 
